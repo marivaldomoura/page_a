8a9e8ed953974f9af896470867a2bcafa2fa0c06
marivaldomoura <mmourasantos12@gmail.com>
2024-02-22 20:22:39 -0300

    treino web site tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    container:{
+      padding:{
+        DEFAULT:'15px',
+      }
+    },
+    screens:{
+      sm:'340px',
+      md:'748px',
+      lg:'960px',
+      xl:'1200px',
+    },
+    extend: {
+      colors:{
+        primary:'#0a1650',
+        hover:'#11d5e5',
+        secondary:'#001a5f',
+        accent:'#0bd4f0',
+        iconcolor:'#3e63a7'
+      },
+      backgroundImage:{
+        hero:'url(../../assets/hero.png)',
+        section:'url(../../assets/section.png)',
+        logo:'url(../../assets/logo.png)',
+        
+      },
+      fontFamily:{
+        primary:'Poppins'
+      },
+
+
+        
+        
+
+      },
+    },
+  plugins: [],
+}
